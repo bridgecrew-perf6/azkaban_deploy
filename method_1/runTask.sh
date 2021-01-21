808275a8386f883fcd13be80f56199809a0e64a0
yang521 <1483885497@qq.com>
2021-01-21 10:15:07 +0800

    Update runTask.sh

--- method_1/runTask.sh
+++ method_1/runTask.sh
@@ -1,3 +1,16 @@
 cd azkaban_deploy/method_1
 
 python -m run_task
+
+
+==========截止==============
+
+./python_venv/bin/python -m python_file.py_name_1 \
+  --mysql-host=${mysql_host} \
+  --mysql-port=${mysql_port} \
+  --mysql-username=${mysql_username} \
+  --mysql-password=${mysql_password} \
+  --mysql-database=${mysql_database2
+./python_venv/bin/python -m python_file.py_name_2 \
+  --es-host=${es_host} \
+  --es-port=${es_port} \
